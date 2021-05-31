a36b1cc033acf4f10ade1be63b7ecb41dffd4c3f
dipa-cotton <diyabunny13@gmail.com>
2021-05-31 15:21:48 -0400

    2 files modified

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>The Sunni Bunniz </h1> <h2>Hi! Welcome to the Sunni Bunniz! We have a youtube channel, where we make all kinds of things! from plushie makeovers to painting shoes. </h2>`;
